d6f6983d9726b50586f4f8a06534a17a4fc66e6f
Argyrou <argykaselve@gmail.com>
2025-05-07 21:33:44 +0200

    amélioration du controller salle

--- app/controllers/sales_controller.ts
+++ app/controllers/sales_controller.ts
@@ -3,22 +3,25 @@ import Sale from '#models/sale'
 import Product from '#models/product'
 
 export default class SalesController {
-  public async index({ view ,auth}: HttpContext) {
-    const sales = await Sale.query().preload('product').where('userid',Number(auth.user?.id))
-    const SALE = (await Sale.query().preload('product').orderBy('created_at', 'desc').where('userid',Number(auth.user?.id)))
-    return view.render('sales/index', { sales , SALE , auth})
+  public async index({ view, auth }: HttpContext) {
+    const sales = await Sale.query().preload('product').where('userid', Number(auth.user?.id))
+    const SALE = await Sale.query()
+      .preload('product')
+      .orderBy('created_at', 'desc')
+      .where('userid', Number(auth.user?.id))
+    return view.render('sales/index', { sales, SALE, auth })
   }
 
-  public async create({ view ,auth}: HttpContext) {
-    const products = await Product.findManyBy('userid',auth.user?.id)
-  
+  public async create({ view, auth }: HttpContext) {
+    const products = await Product.findManyBy('userid', auth.user?.id)
+
     return view.render('sales/create', { products })
   }
 
-  public async store({ request, response,auth }: HttpContext) {
+  public async store({ request, response, auth }: HttpContext) {
     const { product_name, quantity_sold } = request.only(['product_name', 'quantity_sold'])
-    
-    const product = await Product.findByOrFail('name',product_name)
+
+    const product = await Product.findByOrFail('name', product_name)
     if (product.quantity < quantity_sold) {
       throw new Error('Stock insuffisant')
     }
@@ -29,15 +32,13 @@ export default class SalesController {
       quantitySold: quantity_sold,
       totalPrice: totalPrice,
       paymentMethod: 'cash',
-      userid: auth.user?.id
+      userid: auth.user?.id,
     })
 
     product.quantity -= quantity_sold
     await product.save()
 
-    return response.redirect("/sales")
+    return response.redirect('/sales')
   }
-  public async stats({}: HttpContext) {
-    
-  }
+  public async stats({}: HttpContext) {}
 }
