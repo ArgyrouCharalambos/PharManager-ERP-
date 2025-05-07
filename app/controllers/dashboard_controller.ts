340c08fb6ea1abbe5d9b8ac9a9ca4f77c96ea0ab
Argyrou <argykaselve@gmail.com>
2025-05-07 21:35:21 +0200

    amélioration du controller dashboard

--- app/controllers/dashboard_controller.ts
+++ app/controllers/dashboard_controller.ts
@@ -3,9 +3,8 @@ import Sale from '#models/sale'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class DashboardController {
-    public async index ({ view ,auth}:HttpContext) {
-
-      const recentSales = await Sale.query()
+  public async index({ view, auth }: HttpContext) {
+    const recentSales = await Sale.query()
       .orderBy('created_at', 'desc')
       .where('userid', Number(auth.user?.id))
       .limit(5)
@@ -17,13 +16,40 @@ export default class DashboardController {
       }
     })
 
-        const total = (await Product.query().sum('quantity as total').where('userid',Number(auth.user?.id)).first())?.$extras.total || 0
-        const vente = (await Sale.query().sum('total_price as total').where('userid',Number(auth.user?.id)).first())?.$extras.total || 0 
-        const alt = (await Product.query().where('alert_expired', true).where('userid',Number(auth.user?.id)).count('* as total'))[0]?.$extras.total || 0
-        const SALE = (await Sale.query().orderBy('created_at', 'desc').where('userid',Number(auth.user?.id)).limit(5).preload('product'))
-        
-        return view.render('dashboard',{totalStock: total,venteMois:vente,Alerte:alt ,sales:SALE,auth ,saleS })
-      }
-      
-      
-      }
+    const total =
+      (
+        await Product.query()
+          .sum('quantity as total')
+          .where('userid', Number(auth.user?.id))
+          .first()
+      )?.$extras.total || 0
+    const vente =
+      (
+        await Sale.query()
+          .sum('total_price as total')
+          .where('userid', Number(auth.user?.id))
+          .first()
+      )?.$extras.total || 0
+    const alt =
+      (
+        await Product.query()
+          .where('alert_expired', true)
+          .where('userid', Number(auth.user?.id))
+          .count('* as total')
+      )[0]?.$extras.total || 0
+    const SALE = await Sale.query()
+      .orderBy('created_at', 'desc')
+      .where('userid', Number(auth.user?.id))
+      .limit(5)
+      .preload('product')
+
+    return view.render('dashboard', {
+      totalStock: total,
+      venteMois: vente,
+      Alerte: alt,
+      sales: SALE,
+      auth,
+      saleS,
+    })
+  }
+}
